5bb727ce5682b3d3edf352a49e145fbc40725388
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-06-05 09:02:32 -0400

    Added information
    Created post details page
    
    Author:  Erin Cleaver

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from .models import Post
 
@@ -11,3 +11,8 @@ def post_list(request):
         'posts': posts
     }
     return render(request, 'blog/post_list.html', list_of_posts)
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    current_post = {'post': post}
+    return render(request, 'blog/post_details.html', current_post)
